3550cd4f79de151c723ea22c72df9143b95b01f0
Van Son <thanh.duong.vs@gmail.com>
2022-08-19 09:01:12 +0700

    add tab

--- main.go
+++ main.go
@@ -1,55 +1,54 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
+    "context"
+    "fmt"
+    "log"
 
-	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
+    "github.com/gin-gonic/gin"
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
+    "go.mongodb.org/mongo-driver/mongo/readpref"
     "onekiwi.com/api/services"
     "onekiwi.com/api/controllers"
 )
 
 var (
-	server      *gin.Engine
-	us          services.UserService
-	uc          controllers.UserController
-	ctx         context.Context
-	userc       *mongo.Collection
-	mongoclient *mongo.Client
-	err         error
+    server      *gin.Engine
+    us          services.UserService
+    uc          controllers.UserController
+    ctx         context.Context
+    userc       *mongo.Collection
+    mongoclient *mongo.Client
+    err         error
 )
 
 func init() {
-	ctx = context.TODO()
+    ctx = context.TODO()
 
-	mongoconn := options.Client().ApplyURI("mongodb://localhost:27017")
-	mongoclient, err = mongo.Connect(ctx, mongoconn)
-	if err != nil {
-		log.Fatal("error while connecting with mongo", err)
-	}
-	err = mongoclient.Ping(ctx, readpref.Primary())
-	if err != nil {
-		log.Fatal("error while trying to ping mongo", err)
-	}
+    mongoconn := options.Client().ApplyURI("mongodb://localhost:27017")
+    mongoclient, err = mongo.Connect(ctx, mongoconn)
+    if err != nil {
+        log.Fatal("error while connecting with mongo", err)
+    }
+    err = mongoclient.Ping(ctx, readpref.Primary())
+    if err != nil {
+        log.Fatal("error while trying to ping mongo", err)
+    }
 
-	fmt.Println("mongo connection established")
+    fmt.Println("mongo connection established")
 
-	userc = mongoclient.Database("userdb").Collection("users")
-	us = services.NewUserService(userc, ctx)
-	uc = controllers.New(us)
-	server = gin.Default()
+    userc = mongoclient.Database("userdb").Collection("users")
+    us = services.NewUserService(userc, ctx)
+    uc = controllers.New(us)
+    server = gin.Default()
 }
 
 func main() {
-	defer mongoclient.Disconnect(ctx)
+    defer mongoclient.Disconnect(ctx)
 
-	basepath := server.Group("/v1")
-	uc.RegisterUserRoutes(basepath)
+    basepath := server.Group("/v1")
+    uc.RegisterUserRoutes(basepath)
 
-	log.Fatal(server.Run(":9090"))
-
+    log.Fatal(server.Run(":9090"))
 }
